3dbd069f0a7ca0a9ad05f3f7b87051b57da73ac7
kris <kris.walmer@gmail.com>
2016-02-24 10:38:01 -0500

    inital commit

---
+++ src/client/ClientNetworkHandler.java
@@ -0,0 +1,69 @@
+package client;
+
+import com.jme3.network.AbstractMessage;
+import com.jme3.network.Client;
+import com.jme3.network.Message;
+import com.jme3.network.MessageListener;
+import com.jme3.network.Network;
+import com.jme3.network.serializing.Serializer;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import messages.NewClientMessage;
+import messages.Registration;
+
+
+public class ClientNetworkHandler implements MessageListener {
+    private static final String IPADDRESS = "localhost";
+    private static final int PORT = 6143;
+    private Client client;
+    private int ID = -1;
+    ClientNetworkListener gameClient;
+
+    // -------------------------------------------------------------------------
+    public ClientNetworkHandler(ClientNetworkListener gc) {
+        initNetwork();
+       
+        gameClient = gc;
+    }
+
+    // -------------------------------------------------------------------------
+    private void initNetwork() {
+        try {
+            Registration.registerMessages();
+            client = Network.connectToServer(IPADDRESS, PORT);
+            client.start();
+            client.addMessageListener(this);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ClientNetworkHandler.class.getName()).log(Level.SEVERE, "Could not connect to server", ex);
+        }
+    }
+   
+    // -------------------------------------------------------------------------
+    public void messageReceived(Object source, Message msg) {        
+            gameClient.messageReceived(msg);
+         
+         if (msg instanceof NewClientMessage) {
+      // do something with the message
+      NewClientMessage ncm = (NewClientMessage) msg ; 
+      System.out.println(" received: '"+ncm.getString()+"'");
+            
+    }
+    }
+    
+    // -------------------------------------------------------------------------
+    public void send(AbstractMessage msg){
+        if (msg instanceof NewClientMessage){
+        client.send(msg);
+        }
+    }
+    
+    // -------------------------------------------------------------------------
+    public int getID(){
+        if (this.ID == -1){
+            ID = client.getId(); // could still be -1 if not transmitted from server yet
+        }
+        return(ID);
+    }
+}
---
+++ src/server/ServerNetworkListener.java
@@ -0,0 +1,11 @@
+
+package server;
+
+import com.jme3.network.Message;
+
+
+public interface ServerNetworkListener {
+    public void messageReceived(Message msg);
+    
+    public Message newConnectionReceived(int connectionID) throws Exception;
+}
